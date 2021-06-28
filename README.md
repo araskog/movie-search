c5ac9285f12bfc991af882d6c097d5ff79f14336
Amanda <amanda.araskog@gmail.com>
2021-06-28 17:16:09 +0200

    Update readme with instructions. Deploy to Firebase.

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Movie search
 
 Built with React and using the OMDB API
+
+Add an .env file in the root of the project. Assign REACT_APP_API_KEY=[your OMDB API key] in the .env file.
+Run npm install to include dependencies.
--- src/api/omdb.js
+++ src/api/omdb.js
@@ -2,12 +2,12 @@ const API_KEY = process.env.REACT_APP_API_KEY;
 
 export const fetchMovies = (searchValue, pageNumber) => {
   return fetch(
-    `http://www.omdbapi.com/?apikey=${API_KEY}&s=${searchValue}&type="movie"&page=${pageNumber}`
+    `https://www.omdbapi.com/?apikey=${API_KEY}&s=${searchValue}&type="movie"&page=${pageNumber}`
   ).then((response) => response.json());
 };
 
 export const fetchMovieDetails = (movieId) => {
   return fetch(
-    `http://www.omdbapi.com/?apikey=${API_KEY}&i=${movieId}&type="movie"`
+    `https://www.omdbapi.com/?apikey=${API_KEY}&i=${movieId}&type="movie"`
   ).then((response) => response.json());
 };
